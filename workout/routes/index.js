868d6cc0feb6d23a3f6fa7a0d53d48b935ac2f7f
abwelch <abw28@zips.uakron.edu>
2020-10-08 17:29:50 -0500

    clean up routing

--- workout/routes/index.js
+++ workout/routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'We got this!' });
 });
 
+/* GET workout plans page. */
+router.get('/workouts', (req, res) => {
+  res.render('workouts', { title: 'Workout Plans' });
+});
+
 module.exports = router;
--- workout/routes/workouts.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET workout plans page. */
-router.get('/workouts', function(req, res) {
-  res.render('workouts', { title: 'Workout Plans' });
-});
-
-module.exports = router;
