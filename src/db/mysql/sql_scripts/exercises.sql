8befcad7145c792c558bb229f4a467df319429a9
abwelch <abw28@zips.uakron.edu>
2020-10-09 08:34:14 -0500

    Add update

--- src/db/mysql/sql_scripts/exercises.sql
+++ src/db/mysql/sql_scripts/exercises.sql
@@ -1,6 +1,6 @@
 CREATE TABLE exercises (
     id int PRIMARY KEY AUTO_INCREMENT,
-    name varchar(50) NOT NULL,
+    title varchar(50) NOT NULL,
     descr varchar(200) NOT NULL,
     workoutID int NOT NULL,
     FOREIGN KEY (workoutID) REFERENCES workout_plans(id)
