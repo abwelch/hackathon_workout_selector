b671336444a7f1720a0b83f347b125baccf35e79
abwelch <abw28@zips.uakron.edu>
2020-10-09 10:17:42 -0500

    update to include reps and sets attributes

--- src/db/mysql/sql_scripts/exercises.sql
+++ src/db/mysql/sql_scripts/exercises.sql
@@ -2,6 +2,8 @@ CREATE TABLE exercises (
     id int PRIMARY KEY AUTO_INCREMENT,
     title varchar(50) NOT NULL,
     descr varchar(200) NOT NULL,
+    sets int NOT NULL,
+    reps int NOT NULL,
     workoutID int NOT NULL,
     FOREIGN KEY (workoutID) REFERENCES workout_plans(id)
 )
