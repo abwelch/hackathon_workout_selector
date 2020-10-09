46c5b89026f2823ef2c7b760241a1b4abba90cb6
abwelch <abw28@zips.uakron.edu>
2020-10-09 08:33:26 -0500

    correct syntax

--- src/db/mysql/sql_scripts/favorited_workouts.sql
+++ src/db/mysql/sql_scripts/favorited_workouts.sql
@@ -1,7 +1,7 @@
 CREATE TABLE favorited_workouts (
     id int PRIMARY KEY AUTO_INCREMENT,
-    int workoutID NOT NULL,
-    int userID NOT NULL,
+    workoutID int NOT NULL,
+    userID int NOT NULL,
     FOREIGN KEY (workoutID) REFERENCES workout_plans(id),
     FOREIGN KEY (userID) REFERENCES users(id)
 )
--- src/db/mysql/sql_scripts/users.sql
+++ src/db/mysql/sql_scripts/users.sql
@@ -2,5 +2,5 @@ CREATE TABLE users (
     id int PRIMARY KEY AUTO_INCREMENT,
     username varchar(30) NOT NULL,
     email varchar(60) NOT NULL,
-    password varchar(32) NOT NULL,
+    password varchar(32) NOT NULL
 )
